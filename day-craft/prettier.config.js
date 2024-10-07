33b15c6eca8aeeaf565b24099102196dc03e59f2
Restyled.io <commits@restyled.io>
2024-10-07 18:06:24 +0000

    Restyled by clang-format

--- day-craft/prettier.config.js
+++ day-craft/prettier.config.js
@@ -1,6 +1,9 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+/**
+ * @type {import('prettier').Config &
+ *     import('prettier-plugin-tailwindcss').PluginOptions}
+ */
 const config = {
-  plugins: [
+  plugins : [
     "prettier-plugin-tailwindcss",
     "prettier-plugin-organize-imports",
     "prettier-plugin-tailwindcss",
