a50bd18f28875b9dc3edffcf30fdd02ba8bf2883
Jordi García Ventura <jordigarciaventura@gmail.com>
2024-10-22 01:59:07 +0200

    style: format code

--- nextjs-project/app/[locale]/tutorial/page.tsx
+++ nextjs-project/app/[locale]/tutorial/page.tsx
@@ -1,5 +1,5 @@
+import TutorialCarousel from "components/tutorial";
 import { unstable_setRequestLocale } from "next-intl/server";
-import TutorialCarousel from 'components/tutorial';
 
 interface Props {
   params: { locale: string };
@@ -8,7 +8,6 @@ interface Props {
 export default function TutorialPageWrapper({ params: { locale } }: Props) {
   "use client";
   unstable_setRequestLocale(locale);
-
 
   return (
     <>
