742697f1ce2d9e2a37a107715bf5015d7902a4b4
Jordi García Ventura <jordigarciaventura@gmail.com>
2024-10-22 02:07:21 +0200

    fix: missing import

--- nextjs-project/app/[locale]/welcome/page.tsx
+++ nextjs-project/app/[locale]/welcome/page.tsx
@@ -1,14 +1,11 @@
 import ClientWelcomePage from "@/components/ClientWelcomePage";
 import { unstable_setRequestLocale } from "next-intl/server";
-import WelcomePage from 'components/welcome.tsx';
 
 interface Props {
   params: { locale: string };
 }
 
-// Rename the function to avoid conflict
-export default function WelcomePageWrapper({ params: { locale } }: Props) {
-  "use client";
+export default function WelcomePage({ params: { locale } }: Props) {
   unstable_setRequestLocale(locale);
 
   return <ClientWelcomePage />;
