9c8b34e1b3ee39302eaacfae453e406875ccf19c
Oriol-Vilalta <120737270+Oriol-Vilalta@users.noreply.github.com>
2024-11-17 20:57:11 +0100

    Block pages when the user is not logged in

--- nextjs-project/middleware.ts
+++ nextjs-project/middleware.ts
@@ -1,7 +1,23 @@
 import createMiddleware from "next-intl/middleware";
+import { useTranslations } from "next-intl"
 import { routing } from "./i18n/routing";
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+import { requestFormReset } from "react-dom";
 
-export default createMiddleware(routing);
+//export default createMiddleware(routing);
+
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request });
+  console.log()
+  if (!request.cookies.get("next-auth.session-token") && request.nextUrl.pathname !==  ) {
+    return NextResponse.redirect(new URL('', request.url));
+  }
+  console.log(routing)
+
+  const intlMiddleware = createMiddleware(routing);
+  return intlMiddleware(request);
+}
 
 export const config = {
   matcher: [
