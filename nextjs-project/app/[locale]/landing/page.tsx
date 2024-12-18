027f2d6a15856af751777d168abbb0912e86cf6a
Hania Seif <hania.s@eracreators.tech>
2024-12-18 18:53:55 +0100

    landing page

---
+++ nextjs-project/app/[locale]/landing/page.tsx
@@ -0,0 +1,17 @@
+import LandingPage from "@/features/landing/LandingPage";
+import { setRequestLocale } from "next-intl/server";
+
+interface Props {
+  params: { locale: string };
+}
+
+export default function TutorialPageWrapper({ params: { locale } }: Props) {
+  "use client";
+  setRequestLocale(locale);
+
+  return (
+    <>
+      <LandingPage/>
+    </>
+  );
+}
